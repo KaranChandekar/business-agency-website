b92ef849f4e44c3f8b2e5f850e09206cf6b949a9
Karan <karanchandekar431@gmail.com>
2022-08-30 19:45:43 +0530

    made the navbar work

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+  menu.classList.toggle("change");
+});
+// End of Navbar
